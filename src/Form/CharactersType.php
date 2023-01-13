4c5251edd4c94f18c89aa50409dd543780034c87
MartinEwen <68897775+MartinEwen@users.noreply.github.com>
2023-01-13 14:48:12 +0100

    code clean?

--- src/Form/CharactersType.php
+++ src/Form/CharactersType.php
@@ -37,46 +37,7 @@ class CharactersType extends AbstractType
                     'id' => 'classe-select',
                 ],
                 'placeholder' => 'Sélectionnez d\'abord une race',
-            ])
-            // ->add('race', EntityType::class, [
-            //     'class' => Race::class, // va chercher dans la table "Race"
-            //     'choice_label' => 'name', // va chercher dans l'entité "name"
-            //     'choice_value' => 'id',
-            //     'label_attr' => ['class' => 'sr-only'],
-            //     'required' => true,
-            //     'placeholder' => 'Race disponible',
-            // ])
-
-            // ->add('classe', ChoiceType::class, [
-            //     'placeholder' => 'classe dispo',
-
-            // ])
-        ;
-
-
-
-
-
-        // $formModifier = function (FormInterface $form, Race $race = null) {
-        //     $classe = (null === $race) ? [] : $race->getClasses();
-        //     // recupere les classes liée à la race selectionner 
-
-        //     $form->add('classe', EntityType::class, [
-        //         'class' => Classe::class, // va chercher dans la table "Classe"
-        //         'choice_label' => 'name', // va chercher dans l'entité "name"
-        //         'choices' => $classe, // va chercher les réponce recuperer avec getClasses() ??? 
-        //         'placeholder' => 'classe dispo',
-        //         'label' => 'classe'
-        //     ]);
-        // };
-
-        // $builder->get('race')->addEventListener(
-        //     FormEvents::POST_SUBMIT,
-        //     function (FormEvent $event) use ($formModifier) {
-        //         $race = $event->getForm()->getData();
-        //         $formModifier($event->getForm()->getParent(), $race);
-        //     }
-        // );
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
